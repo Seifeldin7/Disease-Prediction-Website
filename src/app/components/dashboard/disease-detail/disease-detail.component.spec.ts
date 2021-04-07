a120ec097474bff000b1adea93aa700e404802a6
Seif Eldin Mohamed <s.seif26@yahoo.com>
2021-04-07 03:44:40 +0200

    improve styles

--- src/app/components/dashboard/disease-detail/disease-detail.component.spec.ts
+++ src/app/components/dashboard/disease-detail/disease-detail.component.spec.ts
@@ -1,25 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('DashboardDetailComponent', () => {
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
